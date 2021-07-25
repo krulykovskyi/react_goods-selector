2f28bb1128638a87459710515efdf0b29a0859df
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-07-25 15:50:07 +0300

    Updates

---
+++ precache-manifest.6b7d731f0d9f10e1d8be0d7085bf0d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314ee638d86c2068515f220b9c4bd539",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4b63c7d496798c18b16",
+    "url": "./static/css/main.680d48ba.chunk.css"
+  },
+  {
+    "revision": "c07500df0ccde94f4968",
+    "url": "./static/js/2.f3cc3ca8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f3cc3ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b63c7d496798c18b16",
+    "url": "./static/js/main.9af97ee5.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
